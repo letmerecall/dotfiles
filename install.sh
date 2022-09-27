e463f19a7e5eba6134ce4b5d54b9c063eb0eec26
letmerecall <girishsharma001@gmail.com>
2022-09-27 15:54:17 +0530

    Update install.sh to install vundle for vim

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash                                                                                                                                                                                                                                                                  
 
+# Install Vundle 
+rm -rf ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 ln -sf $(pwd)/vimrc $HOME/.vimrc
+
+
+vim +PluginInstall +qall
