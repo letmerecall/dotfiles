5ec0b15dd1c39611b9e88d2e76095bca148000b3
letmerecall <girishsharma001@gmail.com>
2022-09-27 15:38:30 +0530

    Add install script to setup .vimrc

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash                                                                                                                                                                                                                                                                  
+
+ln -sf $(pwd)/vimrc $HOME/.vimrc
