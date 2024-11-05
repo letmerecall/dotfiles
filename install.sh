1cbf976cd1592359901af2907dab00eaf4aad558
Girish Sharma <girishsharma001@gmail.com>
2024-11-05 13:00:23 -0500

    Update install script with zsh setup

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash                                                                                                                                                                                                                                                                  
+#!/usr/bin/env bash
 # TODO: Check if vim is installed and if it is compaitable with python
 
-# Install Vundle 
+# Install Vundle
 rm -rf ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -9,3 +9,15 @@ ln -sf $(pwd)/vimrc $HOME/.vimrc
 
 
 vim +PluginInstall +qall
+
+# install zsh
+sudo apt install -y zsh
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+ln -sf $(pwd)/zshrc $HOME/.zshrc
